379938577c9902e71c0ac9410f69d426e275fbd7
Hector Vasquez Perez <luis.vasquez16@tectijuana.edu.mx>
2021-06-28 01:48:37 -0700

    first commit

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 module.exports = {
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB || 'mongodb+srv://vasquez:16210586@cluster0.gvflt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-
-    urlParser : {
+    mongoose.connect(process.env.MONGODB_URI || mongodb+srv://vasquez:16210586@cluster0.gvflt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority),
+    {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
