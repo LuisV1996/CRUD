fe3129b40a4aef12668afba365720c31c79cd723
Hector Vasquez Perez <luis.vasquez16@tectijuana.edu.mx>
2021-06-28 01:28:38 -0700

    first commit

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/mitest2',
+    db: process.env.MONGODB || 'mongodb+srv://vasquez:16210586@cluster0.gvflt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 
     urlParser : {
         useNewUrlParser: true,
